685188dc1fdfb9b019646cdd76cd7e0f5edca298
Joshua Behrens <behrens@heptacom.de>
2021-07-07 22:09:19 +0200

    Add preview configuration

--- src/Resources/flow-component/media.php
+++ src/Resources/flow-component/media.php
@@ -3,15 +3,22 @@ declare(strict_types=1);
 
 use Heptacom\HeptaConnect\Dataset\Ecommerce\Media\Media;
 use Heptacom\HeptaConnect\Portal\Base\Builder\FlowComponent;
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\ConfigurationContract;
 use NiemandOnline\HeptaConnect\Portal\RandomFoxCa\Packer\MediaPacker;
+use NiemandOnline\HeptaConnect\Portal\RandomFoxCa\Portal;
 use NiemandOnline\HeptaConnect\Portal\RandomFoxCa\Support\RandomFoxApiClient;
 use Psr\Http\Message\StreamInterface;
 
 FlowComponent::explorer(Media::class)
-    ->run(static function (RandomFoxApiClient $api, MediaPacker $packer): iterable {
+    ->run(static function (RandomFoxApiClient $api, MediaPacker $packer, ConfigurationContract $config): iterable {
         $i = 1;
+        $max = \PHP_INT_MAX;
 
-        while ($i < \PHP_INT_MAX) {
+        if ($config->get(Portal::CONFIG_PREVIEW)) {
+            $max = ((int) $config->get(Portal::CONFIG_PREVIEW_LIMIT)) + 1;
+        }
+
+        while ($i < $max) {
             $id = (string) $i;
             $blob = $api->getImage($id);
 
