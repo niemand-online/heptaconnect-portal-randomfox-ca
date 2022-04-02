db19108c9c905fcb8b703bcca6cef29bb551d63d
Joshua Behrens <behrens@heptacom.de>
2022-04-02 18:38:22 +0200

    Use configuration binding

--- src/Resources/flow-component/media.php
+++ src/Resources/flow-component/media.php
@@ -3,19 +3,17 @@ declare(strict_types=1);
 
 use Heptacom\HeptaConnect\Dataset\Ecommerce\Media\Media;
 use Heptacom\HeptaConnect\Portal\Base\Builder\FlowComponent;
-use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\ConfigurationContract;
 use NiemandOnline\HeptaConnect\Portal\RandomFoxCa\Packer\MediaPacker;
-use NiemandOnline\HeptaConnect\Portal\RandomFoxCa\RandomFoxCaPortal;
 use NiemandOnline\HeptaConnect\Portal\RandomFoxCa\Support\RandomFoxApiClient;
 use Psr\Http\Message\StreamInterface;
 
 FlowComponent::explorer(Media::class)
-    ->run(static function (RandomFoxApiClient $api, MediaPacker $packer, ConfigurationContract $config): iterable {
+    ->run(static function (RandomFoxApiClient $api, MediaPacker $packer, int $configPreviewLimit, bool $configPreview): iterable {
         $i = 1;
         $max = \PHP_INT_MAX;
 
-        if ($config->get(RandomFoxCaPortal::CONFIG_PREVIEW)) {
-            $max = ((int) $config->get(RandomFoxCaPortal::CONFIG_PREVIEW_LIMIT)) + 1;
+        if ($configPreview) {
+            $max = $configPreviewLimit + 1;
         }
 
         while ($i < $max) {
